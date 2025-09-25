015d6081c4996c1d767e5fe87878e471b27d0e0b
deepa77 <deepsojha77@gmail.com>
2025-09-25 09:34:33 -0400

    upadated by deepa

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("hello we are learning git and git hub");
-		System.out.println("project modified by deepa");
+		System.out.println("hello java class!!");
 	}
 
 }
